8a945bd62e3c9b39a523a0e01dc797f51e7e74e4
blue-sky-dev <bluesea.89125@gmail.com>
2023-06-23 13:12:50 -0700

    updated download_fenhong.py

--- src2/download_FenHong.py
+++ src2/download_FenHong.py
@@ -1,9 +1,52 @@
 import os
-import json
+import json, requests
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-import FenHong.url
 
+def get_list(CQCXR='2000-01-01',pn=1,pz=200):
+    siteURL = 'https://datacenter-web.eastmoney.com/api/data/v1/get'
+    para = {
+        'reportName' : 'RPT_DATE_SHAREBONUS_DET',
+        'columns' : 'ALL',
+        'quoteColumns' : '',
+        'pageNumber' : '1',
+        'sortColumns' : 'REPORT_DATE',
+        'sortTypes' : '-1',
+        'source' : 'WEB',
+        'client' : 'WEB',
+        '_' : '1686592227740'
+    }
+    url = requests.Request('GET', url=siteURL, params=para).prepare().url
+    return fetch_push(url)
+
+def get_data(report_date):
+    siteURL = 'https://datacenter-web.eastmoney.com/api/data/v1/get'
+    para = {
+        'sortColumns' : 'PLAN_NOTICE_DATE',
+        'sortTypes' : '-1',
+        'pageNumber' : '1',
+        'reportName' : 'RPT_SHAREBONUS_DET',
+        'columns' : 'ALL',
+        'quoteColumns' : '',
+        'js' : '{"data":(x),"pages":(tp)}',
+        'source' : 'WEB',
+        'client' : 'WEB',
+        'filter' : "(REPORT_DATE='" + report_date + "')"
+    }
+    url = requests.Request('GET', url=siteURL, params=para).prepare().url
+    return fetch_push(url)
+
+def fetch_push(url): 
+    r = requests.get(url, 
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML  '
+                            'like Gecko) Chrome/81.0.4044.138 Safari/537.36 Edg/81.0.416.77'
+        }, 
+        timeout=10
+    )
+    print(r.content)
+    js = json.loads(r.content)
+    return js
 
 if __name__ == '__main__':
     # checking if download folder exists or not.
@@ -14,20 +57,26 @@ if __name__ == '__main__':
 
     one_yrs_ago = datetime.now() - relativedelta(years=10)
     last_date_limit = one_yrs_ago.strftime('%Y-%m-%d')
-    response = FenHong.url.get_list()
+    response = get_list()
     list = response['result']['data']
     for x in list:
         report_date = datetime.strptime(x['REPORT_DATE'], "%Y-%m-%d %H:%M:%S").strftime('%Y-%m-%d')
         prefix_filename = datetime.strptime(x['REPORT_DATE'], "%Y-%m-%d %H:%M:%S").strftime('%Y%m%d')
         print("downloading and writing text file for " + report_date + " report")
-        data_response = FenHong.url.get_data(report_date)
-        for y in data_response['result']['data']:
-            security_code = y['SECURITY_CODE']
-            text = json.dumps(y)
-            script_dir = os.path.dirname(__file__)
-            rel_path = "../download/FENHONG/FENHONG_" + prefix_filename + "_" + security_code +".txt"
-            abs_file_path = os.path.join(script_dir, rel_path)
-            with open(abs_file_path, "w") as f:
-                f.write(text)
+        data_response = get_data(report_date)
+        text = json.dumps(y)
+        script_dir = os.path.dirname(__file__)
+        rel_path = "../download/FENHONG/FENHONG_" + prefix_filename + ".txt"
+        abs_file_path = os.path.join(script_dir, rel_path)
+        with open(abs_file_path, "w") as f:
+            f.write(text)
+        # for y in data_response['result']['data']:
+        #     security_code = y['SECURITY_CODE']
+        #     text = json.dumps(y)
+        #     script_dir = os.path.dirname(__file__)
+        #     rel_path = "../download/FENHONG/FENHONG_" + prefix_filename + "_" + security_code +".txt"
+        #     abs_file_path = os.path.join(script_dir, rel_path)
+        #     with open(abs_file_path, "w") as f:
+        #         f.write(text)
         
 
