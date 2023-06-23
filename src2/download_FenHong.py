4513f3a647c4e677365d57f1713cbbf73d7b9a90
blue-sky-dev <bluesea.89125@gmail.com>
2023-06-23 13:21:15 -0700

    download fenhong data for only 3 years

--- src2/download_FenHong.py
+++ src2/download_FenHong.py
@@ -59,17 +59,23 @@ if __name__ == '__main__':
     last_date_limit = one_yrs_ago.strftime('%Y-%m-%d')
     response = get_list()
     list = response['result']['data']
+    # downloading FenHong database for only recent 3 years.
+    count = 1
     for x in list:
         report_date = datetime.strptime(x['REPORT_DATE'], "%Y-%m-%d %H:%M:%S").strftime('%Y-%m-%d')
         prefix_filename = datetime.strptime(x['REPORT_DATE'], "%Y-%m-%d %H:%M:%S").strftime('%Y%m%d')
         print("downloading and writing text file for " + report_date + " report")
         data_response = get_data(report_date)
-        text = json.dumps(y)
+        text = json.dumps(data_response)
         script_dir = os.path.dirname(__file__)
         rel_path = "../download/FENHONG/FENHONG_" + prefix_filename + ".txt"
         abs_file_path = os.path.join(script_dir, rel_path)
         with open(abs_file_path, "w") as f:
             f.write(text)
+        if (count < 6):
+            count += 1
+        else:
+            break
         # for y in data_response['result']['data']:
         #     security_code = y['SECURITY_CODE']
         #     text = json.dumps(y)
