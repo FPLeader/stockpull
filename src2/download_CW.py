d2a614e1d61af78711b498b704add828c880b8ec
blue-sky-dev <bluesea.89125@gmail.com>
2023-06-28 20:45:30 -0700

    added full CW download feature.

--- src2/download_CW.py
+++ src2/download_CW.py
@@ -30,7 +30,7 @@ def make_text_file(filename, data):
     script_dir = os.path.dirname(__file__)
     rel_path = "../download/CW/" + filename + ".txt"
     abs_file_path = os.path.join(script_dir, rel_path)
-    with open(abs_file_path, "w") as f:
+    with open(abs_file_path, "w", encoding='utf-8') as f:
         f.write(text)
     print(filename + " file was made successfully!")
 
@@ -57,7 +57,7 @@ def url_CW_html(js):
         return ctype
     except: return
 
-def url_ZCFZB2(ide='0000012', companyType=4, dates='2023-03-31'):
+def get_ZCFZB2(ide='0000012', companyType=4, dates='2023-03-31'):
     siteURL = 'http://emweb.eastmoney.com/PC_HSF10/NewFinanceAnalysis/zcfzbAjaxNew'
     para = {
             'companyType': companyType,
@@ -78,6 +78,79 @@ def url_ZCFZB2(ide='0000012', companyType=4, dates='2023-03-31'):
     return js
     # print(r.content)
 
+def get_XJLLB2(ide='0000012',companyType=4,dates="2023-03-31"):
+    siteURL = 'http://emweb.eastmoney.com/PC_HSF10/NewFinanceAnalysis/xjllbAjaxNew'
+    para = {
+            'companyType': companyType,
+            'reportDateType': 0,
+            'reportType':1,
+            'code': EM_TOOL.IDE2SID(ide),
+            'dates': dates,
+    }
+    url = requests.Request('GET', url=siteURL, params=para).prepare().url
+    r = requests.get(url,
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML  '
+                            'like Gecko) Chrome/81.0.4044.138 Safari/537.36 Edg/81.0.416.77'
+        }, 
+        timeout=10
+    )
+    js = json.loads(r.content)
+    return js
+
+def get_LRB2(ide='0000012',companyType=4,dates="2000-01-01"):
+    siteURL = 'http://emweb.eastmoney.com/PC_HSF10/NewFinanceAnalysis/lrbAjaxNew'
+    para = {
+            'companyType': companyType,
+            'reportDateType': 0,
+            'reportType':1,
+            'code': EM_TOOL.IDE2SID(ide),
+            'dates': dates,
+    }
+    url = requests.Request('GET', url=siteURL, params=para).prepare().url
+    r = requests.get(url,
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML  '
+                            'like Gecko) Chrome/81.0.4044.138 Safari/537.36 Edg/81.0.416.77'
+        }, 
+        timeout=10
+    )
+    js = json.loads(r.content)
+    return js
+
+def get_ZYZ2(ide='0000012'):
+    siteURL = 'http://emweb.eastmoney.com/PC_HSF10/NewFinanceAnalysis/ZYZBAjaxNew'
+    para = {
+        'type': 0,
+        'code': EM_TOOL.IDE2SID(ide)
+    }
+    url = requests.Request('GET', url=siteURL, params=para).prepare().url
+    r = requests.get(url,
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML  '
+                            'like Gecko) Chrome/81.0.4044.138 Safari/537.36 Edg/81.0.416.77'
+        }, 
+        timeout=10
+    )
+    js = json.loads(r.content)
+    return js
+
+def get_DBFX2(ide='0000012'):
+    siteURL = 'http://emweb.eastmoney.com/PC_HSF10/NewFinanceAnalysis/DBFXAjaxNew'
+    para = {
+        'code': EM_TOOL.IDE2SID(ide)
+    }
+    url = requests.Request('GET', url=siteURL, params=para).prepare().url
+    r = requests.get(url,
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML  '
+                            'like Gecko) Chrome/81.0.4044.138 Safari/537.36 Edg/81.0.416.77'
+        }, 
+        timeout=10
+    )
+    js = json.loads(r.content)
+    return js
+
 if __name__ == "__main__":
     # checking if download folder exists or not.
     if not os.path.isdir("../download"):
@@ -93,6 +166,15 @@ if __name__ == "__main__":
         # stock ctype = 1
         dates = get_lastday_of_quarter()
         ctype = url_CW_html(index)
-        ZCFZB2_result = url_ZCFZB2(ide=index['IDE'], companyType=ctype, dates=dates)
+        ZCFZB2_result = get_ZCFZB2(ide=index['IDE'], companyType=ctype, dates=dates)
         make_text_file("ZCFZB2_" + index['IDE'], ZCFZB2_result)
+        XJLLB2_result = get_XJLLB2(ide=index['IDE'], companyType=ctype, dates=dates)
+        make_text_file("XJLLB2_" + index['IDE'], XJLLB2_result)
+        LRB2_result = get_LRB2(ide=index['IDE'], companyType=ctype, dates=dates)
+        make_text_file("LRB2_" + index['IDE'], LRB2_result)
+        ZYZ2_result = get_ZYZ2(ide=index['IDE'])
+        make_text_file("ZYZ2_" + index['IDE'], ZYZ2_result)
+        DBFX2_result = get_DBFX2(ide=index['IDE'])
+        print(DBFX2_result)
+        make_text_file("DBFX2_" + index['IDE'], DBFX2_result)
         break
